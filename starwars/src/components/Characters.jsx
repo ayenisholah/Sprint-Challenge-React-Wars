26ac7db2e6f3d912259377f96d0d3bb43811a0dd
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-07 12:57:18 +0100

    Charactersrefactor

--- starwars/src/components/Characters.jsx
+++ starwars/src/components/Characters.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
-
 import './StarWars.css';
 
-class Characters extends React.Component {
-    constructor() {
-        super();
-    }
+// class Characters extends React.Component {
+//     constructor() {
+//         super();
+//     }
 
-    render() {
-        return  (
-            <div className='characters'>
-                <div className='character-name'>
-                    <h2>Name: {this.props.name}</h2>
-                </div>
-                <div className='character-details'>
-                    <p>Gender: {this.props.gender}</p>
-                    <p>Height: {this.props.height}</p>
-                    <p>Mass: {this.props.mass}</p>
-                    <p>Eye Color: {this.props.eye_color}</p>
-                    <p>Hair Color: {this.props.hair_color}</p>
-                </div>
+//     render() {
+//         return  (
+//             <div className='characters'>
+//                 <div className='character-name'>
+//                     <h2>Name: {this.props.name}</h2>
+//                 </div>
+//                 <div className='character-details'>
+//                     <p>Gender: {this.props.gender}</p>
+//                     <p>Height: {this.props.height}</p>
+//                     <p>Mass: {this.props.mass}</p>
+//                     <p>Eye Color: {this.props.eye_color}</p>
+//                     <p>Hair Color: {this.props.hair_color}</p>
+//                 </div>
     
-            </div>
-        );
-    }
-}
+//             </div>
+//         );
+//     }
+// }
 
+function Characters(props) {
+    return (
+        <div className='characters'>
+            <div className='character-name'>
+                <h2>Name: {props.name}</h2>
+            </div>
+            <div className='character-details'>
+                <p>Gender: {props.gender}</p>
+                <p>Height: {props.height}</p>
+                <p>Mass: {props.mass}</p>
+                <p>Eye Color: {props.eye_color}</p>
+                <p>Hair Color: {props.hair_color}</p>
+            </div>
+        </div>
+
+    )
+}
 
 export default Characters;
