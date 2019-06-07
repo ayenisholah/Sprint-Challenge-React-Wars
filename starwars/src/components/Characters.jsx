dfe040840f1f31f2476e11a4f7f63d254a6b8b53
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-07 13:48:53 +0100

    .

--- starwars/src/components/Characters.jsx
+++ starwars/src/components/Characters.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
 import './StarWars.css';
 
-// class Characters extends React.Component {
-//     constructor() {
-//         super();
-//     }
-
-//     render() {
-//         return  (
-//             <div className='characters'>
-//                 <div className='character-name'>
-//                     <h2>Name: {this.props.name}</h2>
-//                 </div>
-//                 <div className='character-details'>
-//                     <p>Gender: {this.props.gender}</p>
-//                     <p>Height: {this.props.height}</p>
-//                     <p>Mass: {this.props.mass}</p>
-//                     <p>Eye Color: {this.props.eye_color}</p>
-//                     <p>Hair Color: {this.props.hair_color}</p>
-//                 </div>
-    
-//             </div>
-//         );
-//     }
-// }
-
 function Characters(props) {
     return (
         <div className='characters'>
